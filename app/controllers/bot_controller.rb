b2564b6516a6d7ce7e86252d9b963389a3bb543b
Naing Lu <moemyintkoko99@gmail.com>
2016-07-07 12:13:47 +0630

    a

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -5,12 +5,11 @@ class BotController < ApplicationController
   def webhook
     if params["hub.verify_token"] == "my_voice_is_my_password_verify_me"
       render json: params["hub.challenge"]
+    end
+    unless params["entry"].nil? || params["entry"].empty?
+      sender = params["entry"][0]["messaging"][0]["sender"]["id"]
 
-      unless params["entry"].nil? || params["entry"].empty?
-        sender = params["entry"][0]["messaging"][0]["sender"]["id"]
-
-        FacebookBot.new.send_text_message(sender, "Hello")
-      end
+      FacebookBot.new.send_text_message(sender, "Hello")
     end
     render :nothing => true, :status => 200, :content_type => 'text/html'
   end
