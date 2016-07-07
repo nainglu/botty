1a85961996662dec445d4b3046ba17db401bce5a
Naing Lu <moemyintkoko99@gmail.com>
2016-07-07 11:46:33 +0630

    unless

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -3,11 +3,13 @@ class BotController < ApplicationController
   protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 
   def webhook
-    sender = params["entry"][0]["messaging"][0]["sender"]["id"]
-    text = params["entry"][0]["messaging"][0]["message"]["text"]
-    
-    FacebookBot.new.send_text_message(sender, "Hello")
 
+    unless params["entry"].nil? || params["entry"].empty?
+      sender = params["entry"][0]["messaging"][0]["sender"]["id"]
+      text = params["entry"][0]["messaging"][0]["message"]["text"]
+
+      FacebookBot.new.send_text_message(sender, "Hello")
+    end
     render :nothing => true, :status => 200, :content_type => 'text/html'
   end
 
