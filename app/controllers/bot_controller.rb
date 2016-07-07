8491e9abd2206c1ea69ebd574240aca112b69409
Naing Lu <moemyintkoko99@gmail.com>
2016-07-07 10:02:00 +0630

    p

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -7,8 +7,9 @@ class BotController < ApplicationController
 
     unless params["entry"].nil? || params["entry"].empty?
       sender = params["entry"][0]["messaging"][0]["sender"]["id"]
-      res = FacebookBot.new.send_text_message(sender, "Hi thanks for messaging")
+      FacebookBot.new.send_text_message(sender, "Hi thanks for messaging")
     end
   end
 
 end
+
