baa99212aba482edf1392bccb52e68611d4dc670
Naing Lu <moemyintkoko99@gmail.com>
2016-07-07 10:20:55 +0630

    idk

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -1,18 +1,15 @@
 class BotController < ApplicationController
 
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 
   def webhook
     if params['hub.verify_token'] == 'my_voice_is_my_password_verify_me'
       render json: params['hub.challenge']
     end
 
-    unless params["entry"].nil? || params["entry"].empty?
-      sender = params["entry"][0]["messaging"][0]["sender"]["id"]
-      FacebookBot.new.send_text_message(sender, "Hi thanks for messaging")
-    end
+    sender = params["entry"][0]["messaging"][0]["sender"]["id"]
+    res = FacebookBot.new.send_text_message(sender, "Hi thanks for messaging")
 
-    render :nothing => true, :status => 200, :content_type => 'text/html'
   end
 
 end
