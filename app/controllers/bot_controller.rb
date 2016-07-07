1eb16f4c07beac17cc668ebcada625d64d5716c2
Naing Lu <moemyintkoko99@gmail.com>
2016-07-07 09:50:20 +0630

    check if params entry nil

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -4,8 +4,11 @@ class BotController < ApplicationController
     if params['hub.verify_token'] == 'my_voice_is_my_password_verify_me'
       render json: params['hub.challenge']
     end
-    sender = params["entry"][0]["messaging"][0]["sender"]["id"]
-    res = FacebookBot.new.send_text_message(sender, "Hi thanks for messaging")
+
+    unless params["entry"].nil? || params["entry"].empty?
+      sender = params["entry"][0]["messaging"][0]["sender"]["id"]
+      res = FacebookBot.new.send_text_message(sender, "Hi thanks for messaging")
+    end
   end
 
 end
