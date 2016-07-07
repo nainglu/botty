be0d7245e962940f0c71c88021ea0aa905df3b97
Naing Lu <moemyintkoko99@gmail.com>
2016-07-07 11:34:48 +0630

    sender

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -3,6 +3,8 @@ class BotController < ApplicationController
   protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 
   def webhook
+    sender = params["entry"][0]["messaging"][0]["sender"]["id"]
+    text = params["entry"][0]["messaging"][0]["message"]["text"]
     
     FacebookBot.new.send_text_message(sender, "Hello")
 
