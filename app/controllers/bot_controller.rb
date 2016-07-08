eb260b484396e04dbe3748c8c132566623176c49
Naing Lu <moemyintkoko99@gmail.com>
2016-07-08 12:37:22 +0630

    a

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -3,7 +3,7 @@ class BotController < ApplicationController
   protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 
   def webhook
-    if params["hub.verify_token"] == "my_voice_is_my_password_verify_me"
+    if params["hub.verify_token"] == "my_voice_is_my_voice_verify_me"
       render json: params["hub.challenge"]
     end
     unless params["entry"].nil? || params["entry"].empty?
