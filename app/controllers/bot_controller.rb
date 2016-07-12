41872820adcb3d7433b5a67b3e3a2fbfdde6596d
Naing Lu <moemyintkoko99@gmail.com>
2016-07-12 17:17:17 +0630

    a

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -7,42 +7,38 @@ class BotController < ApplicationController
       render json: params["hub.challenge"]
     end
     unless params["entry"].nil? || params["entry"].empty?
+      
+      sender = params["entry"][0]["messaging"][0]["sender"]["id"]
+      
       unless params["entry"][0]["messaging"][0]["message"].nil?
-        sender = params["entry"][0]["messaging"][0]["sender"]["id"]
         text = params["entry"][0]["messaging"][0]["message"]["text"]
-        if !params["entry"][0]["messaging"][0]["postback"].nil?
-          payload = params["entry"][0]["messaging"][0]["postback"]["payload"]
-          FacebookBot.new.send_text_message(sender, payload)
-        end
+      else 
+        text = params["entry"][0]["messaging"][0]["postback"]["payload"]
+      end
+
         if greeting.include? text
           FacebookBot.new.send_generic_message(sender, topic_bubble)
+        elsif text == "Website Design"
+          FacebookBot.new.send_generic_message(sender, web_pack_quick_reply)
+        elsif text == "Basic"
+          FacebookBot.new.send_generic_message(sender, generic_template)
+        elsif text == "Website Hosting"
+          res = "Here you go!"
+          mes = {
+            "attachment":{
+              "type":"image",
+              "payload":{
+                "url":"http://www.gstatic.com/webp/gallery/2.jpg"
+              }
+            }
+          }
+          FacebookBot.new.send_text_message(sender, res)
+          FacebookBot.new.send_generic_message(sender, mes)
+        elsif text == "Email & Domain Registration"
+          FacebookBot.new.send_generic_message(sender, bubble_template)
         else
           res = "Okay! we are out of sense. let's get back to conversation. please say hi!"
           FacebookBot.new.send_text_message(sender, res)
-        end
-      end
-      if params["entry"][0]["messaging"][0]["postback"]
-          payload = params["entry"][0]["messaging"][0]["postback"]["payload"]
-          FacebookBot.new.send_text_message(sender, payload)
-          if payload == "Website Design"
-            FacebookBot.new.send_generic_message(sender, web_pack_quick_reply)
-          elsif payload == "Basic"
-            FacebookBot.new.send_generic_message(sender, generic_template)
-          elsif payload == "Website Hosting"
-            res = "Here you go!"
-            mes = {
-              "attachment":{
-                "type":"image",
-                "payload":{
-                  "url":"http://www.gstatic.com/webp/gallery/2.jpg"
-                }
-              }
-            }
-            FacebookBot.new.send_text_message(sender, res)
-            FacebookBot.new.send_generic_message(sender, mes)
-          elsif payload == "Email & Domain Registration"
-            FacebookBot.new.send_generic_message(sender, bubble_template)
-          end
         end
     end
     render :nothing => true, :status => 200, :content_type => 'text/html'
