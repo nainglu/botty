4ba408f2c44d42a47001ae35bafa6da49088f0f5
Naing Lu <moemyintkoko99@gmail.com>
2016-07-13 16:06:58 +0630

    a

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -55,7 +55,7 @@ class BotController < ApplicationController
             "attachment":{
               "type":"image",
               "payload":{
-                "url":"http://www.gstatic.com/webp/gallery/2.jpg"
+                "url":"http://webfactorymm.com/app/assets/images/web_pack_standard.jpg"
               }
             }
           }
@@ -68,7 +68,7 @@ class BotController < ApplicationController
             "attachment":{
               "type":"image",
               "payload":{
-                "url":"http://www.gstatic.com/webp/gallery/2.jpg"
+                "url":"http://webfactorymm.com/app/assets/images/web_pack_premium.jpg"
               }
             }
           }
